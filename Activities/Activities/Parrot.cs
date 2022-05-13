309f0365c84da866522f4f1ac2104f71bf4f5216
Ia <iavaquilar@gmail.com>
2022-05-13 22:12:18 +0100

    Completed activity 1 to 4

---
+++ Activities/Activities/Parrot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activities
+{
+    public class Parrot : Bird
+    {
+        public Parrot(string name, string color) : base(name, color)
+        {
+        }
+        public override void Speak()
+        {
+            Console.WriteLine("A funny message!");
+        }
+    }
+}
