b847b53ec13ca488344c8068b851a0f5446a4cac
Ia <iavaquilar@gmail.com>
2022-05-15 22:31:06 +0100

    Added UML diagrams and exercise 5 completed.

---
+++ Activities/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Activities;
+
+Pigeon pippa = new Pigeon("Pippa", "Grey");
+pippa.Speak();
+pippa.Fly();
+pippa.EatPizza();
+pippa.Spin();
+pippa.DoTheCaterpillar();
+pippa.Jump();
+
+
+Penguin pingu = new Penguin("Pingu", "Black & White");
+pingu.Speak();
+pingu.Fly();
+pingu.Spin();
+pingu.DoTheCaterpillar();
+pingu.Jump();
+
+
+Parrot parroty = new Parrot("Parroty", "Black & White");
+parroty.Speak();
